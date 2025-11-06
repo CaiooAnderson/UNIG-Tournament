739e9a8b11c19d5230fa3a89f76884e65ad83dcc
CaiozinS2 <caioandersongoes@gmail.com>
2025-11-06 17:17:31 -0300

    Pasta front-end configurada com React + Vite + TailwindCSS + MaterialUI + TypeScript

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-dvh flex justify-center items-center">
+        <Button variant="contained" className="text-3xl font-bold">
+          Botão de teste
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
